3dfb7cde2410d3a06f156c63ac10b4a9b31c0d7f
kyler <kyler@pop-os.localdomain>
2019-10-15 08:22:00 -0600

    this is the hello world commit for our electon app, it shows a single page and does nothing else

---
+++ App/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
