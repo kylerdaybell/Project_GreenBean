31751564a4ce00546de16271a48b3349836afaa8
kyler <kyler@pop-os.localdomain>
2019-10-15 10:06:33 -0600

    this isnt working

--- App/index.js
+++ App/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const fs = require("fs");
 
 const electron = require('electron')
 const {app, BrowserWindow} = electron
@@ -7,9 +8,10 @@ var mainWindow = null;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
+        resizable: true,
         height: 600,
         width: 800
     });
 
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/pages/index.html');
 });
