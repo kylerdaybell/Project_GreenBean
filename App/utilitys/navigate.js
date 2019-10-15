16920ead0f1b00573b2fd1726a1ef608be6aa76c
kyler <kyler@pop-os.localdomain>
2019-10-15 10:27:49 -0600

    did some basic styling

--- App/utilitys/navigate.js
+++ App/utilitys/navigate.js
@@ -1,8 +1,3 @@
-var remote = require('electron').remote;
-var electronFs = remote.require('fs');
-
 function Navigate() {
-    document.getElementById("content-area").innerHTML = "why does this still not work";
-
-    document.getElementById("content-area").innerHTML = electronFs.readFileSync('/pages/secondpage.html');
+    document.getElementById("content-area").innerHTML = "you changed the content area";
 }
