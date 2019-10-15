472ec029fa1fcd70b73732ad60716ddb4f33745f
kyler <kyler@pop-os.localdomain>
2019-10-15 11:02:20 -0600

    the navigation is broken down into smaller functions

--- App/utilitys/navigate.js
+++ App/utilitys/navigate.js
@@ -1,7 +1,24 @@
 function Navigate(pagename) {
     if(pagename =="home"){
-        document.getElementById("content-area").innerHTML = "<h1>welcome home</h1>";
+        SetHomePage()
     }else if(pagename == "search"){
-        document.getElementById("content-area").innerHTML = "<h1>Search for recipes</h1>"
+        SetSearchPage();
+    }else if(pagename == "new"){
+        SetNewPage();
     }       
+}
+
+function SetHomePage(){
+    document.getElementById("title").innerHTML = "<h1>Welcome Home</h1>";
+    document.getElementById("content-area").innerHTML = "<p>this is where the home page goes</p>";
+}
+
+function SetSearchPage(){
+    document.getElementById("title").innerHTML = "<h1>Search for Recipes</h1>"
+    document.getElementById("content-area").innerHTML = "<p>this is where the search page goes</p>";
+}
+
+function SetNewPage(){
+    document.getElementById("title").innerHTML = "<h1>Add a new recipe</h1>"
+    document.getElementById("content-area").innerHTML = "<p>this is where the add page goes</p>";
 }
