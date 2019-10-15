6746a6cac095c36d2af88f1f49458b6fc918900f
kyler <kyler@pop-os.localdomain>
2019-10-15 10:43:44 -0600

    so basicaly navigation works but not in the way that i hoped it would

--- App/utilitys/navigate.js
+++ App/utilitys/navigate.js
@@ -1,3 +1,7 @@
-function Navigate() {
-    document.getElementById("content-area").innerHTML = "you changed the content area";
+function Navigate(pagename) {
+    if(pagename =="home"){
+        document.getElementById("content-area").innerHTML = "<h1>welcome home</h1>";
+    }else if(pagename == "search"){
+        document.getElementById("content-area").innerHTML = "<h1>Search for recipes</h1>"
+    }       
 }
