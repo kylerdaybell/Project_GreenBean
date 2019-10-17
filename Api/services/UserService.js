fc4f7a5c8f075c4d77ea1f6eba4a419d094c05a3
Brandon <43394345+SirNodnarb@users.noreply.github.com>
2019-10-16 18:06:12 -0600

    Update UserService.js

--- Api/services/UserService.js
+++ Api/services/UserService.js
@@ -1,12 +1,12 @@
 var IApiDatabaseService;
-const User = require('../models/UserModel.js')
+var User = require('../models/UserModel.js')
 
-const UserService = {
+var UserService = {
     constructor: function(IApiDatabaseServiceInsert){
         IApiDatabaseService = IApiDatabaseServiceInsert;
     },
     RegisterUser: async function(user){
-        let databaseResult = IApiDatabaseService.GetUser(user.email);
+        var databaseResult = IApiDatabaseService.GetUser(user.email);
         if(databaseResult[0]['EMAIL'] === null){
             IApiDatabaseService.RegisterUser(user);
             return true;
