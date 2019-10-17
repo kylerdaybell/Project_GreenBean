724ed9c34c91a44b29265d7326456d8cf30aa8d1
Kyler Daybell <45365753+kylerdaybell@users.noreply.github.com>
2019-10-17 16:28:11 -0600

    Update UserService.js

--- Api/services/UserService.js
+++ Api/services/UserService.js
@@ -1,6 +1,6 @@
 var IApiDatabaseService;
 var User = require('../models/UserModel.js')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 
 var UserService = {
     constructor: function(IApiDatabaseServiceInsert){
