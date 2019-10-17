9b224bd3ed923f6ac10423c5398d43559d80998b
Kyler Daybell <45365753+kylerdaybell@users.noreply.github.com>
2019-10-17 16:18:27 -0600

    Update UserService.js

--- Api/services/UserService.js
+++ Api/services/UserService.js
@@ -12,6 +12,18 @@ var UserService = {
             return true;
         }
         return false;
+    },
+    LoginUser: async function(user){
+        var databaseResult = await IApiDatabaseService.GetUser(user.email);
+        if(typeof databaseResult[0] != 'undefined'){
+            if(bcrypt.compareSync(databaseResult[0]['PASSWORD'],user.password)){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 }
 
