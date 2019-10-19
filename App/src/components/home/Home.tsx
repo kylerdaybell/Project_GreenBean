7aee47d493af79956365f696ac6455da723b3958
Brandon Isbell <brandonisbell1997@gmail.com>
2019-10-19 14:21:27 -0600

    Electron with React

---
+++ App/src/components/home/Home.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import "../../css/w3.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div id="title" className="w3-container w3-green">
+        <h1>Welcome Home</h1>
+      </div>
+      <div id="content-area" className="w3-container">
+        <p>this is where the home page goes</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
