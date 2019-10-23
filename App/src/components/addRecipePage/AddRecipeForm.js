4ac08aaf845e41edf24ac45f2683980264eef8f2
kyler <kyler@pop-os.localdomain>
2019-10-23 09:21:22 -0600

    made some style changes to the add recipe form

--- App/src/components/addRecipePage/AddRecipeForm.js
+++ App/src/components/addRecipePage/AddRecipeForm.js
@@ -37,22 +37,22 @@ const AddRecipeForm = () => {
       
         <legend>Add a new Recipe:</legend>
         <label htmlFor="name">Name:</label>
-        <input id="name" type="text" />
+        <input class="w3-input w3-border" id="name" type="text" />
         <br />
         <label htmlFor="description">Description:</label>
-        <input id="description" type="text" />
+        <input class="w3-input w3-border" id="description" type="text" />
         <br />
         <label htmlFor="picture">Picture:</label>
         <input id="picture" type="file" />
         <br />
         <label htmlFor="prep-time">Prep-Time:</label>
-        <input id="prep-time" type="text" />
+        <input class="w3-input w3-border" id="prep-time" type="text" />
         <br />
         <label htmlFor="cook-time">Cook-Time:</label>
-        <input id="cook-time" type="text" />
+        <input class="w3-input w3-border" id="cook-time" type="text" />
         <br />
         <label htmlFor="instructions">Instructions:</label>
-        <input id="instructions" type="text" />
+        <input class="w3-input w3-border" id="instructions" type="text" />
         <br />
         
     
--- App/src/components/addRecipePage/IngredientAdd.js
+++ App/src/components/addRecipePage/IngredientAdd.js
@@ -22,23 +22,23 @@ const IngredientAdd = props => {
 
   return (
     <>
-      <div>
-        <input onChange={(event) => setName(event.target.value)} type="text" placeholder="name" value={name}/>
-        <input onChange={(event) => setAmount(Number(event.target.value))} type="number" placeholder="amount" value={amount}/>
-        <input onChange={(event) => setUnit(event.target.value)} type="text" placeholder="unit" value={unit}/>
-        <button onClick={() => addIngredient()}>Add</button>
+      <div class="w3-row">
+        <input  class="w3-input w3-quarter w3-border" onChange={(event) => setName(event.target.value)} type="text" placeholder="name" value={name}/>
+        <input  class="w3-input w3-quarter w3-border" onChange={(event) => setAmount(Number(event.target.value))} type="number" placeholder="amount" value={amount}/>
+        <input  class="w3-input w3-quarter w3-border" onChange={(event) => setUnit(event.target.value)} type="text" placeholder="unit" value={unit}/>
+        <button class = "w3-btn w3-hover-green"onClick={() => addIngredient()}>Add</button>
       </div>
       {ingredientArray.map((ingredient) => (
         <div >
           <span>
             {ingredient.name} {ingredient.amount} {ingredient.unit}/s
           </span>
-          <button onClick={()=>removeIngredient(ingredient)}>Delete</button>
+          <button class="w3-button w3-hover-red"onClick={()=>removeIngredient(ingredient)}>remove</button>
           <br />
         </div>
       ))}
 
-      <button onClick={() => props.onSubmit(ingredientArray)} >Submit</button>
+      <button class="w3-btn w3-green " onClick={() => props.onSubmit(ingredientArray)}>create recipe</button>
     </>
   );
 };
