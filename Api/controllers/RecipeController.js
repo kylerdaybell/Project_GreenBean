0f323f746f3424b9763a15d79dff08cbf9267f21
kyler <kyler@pop-os.localdomain>
2019-10-30 09:57:31 -0600

    working on extracting the data from the database by ingredient name

--- Api/controllers/RecipeController.js
+++ Api/controllers/RecipeController.js
@@ -69,7 +69,7 @@ var RecipeController = {
     },
     GetRecipeByIngredientsList: async function(req,res){
         IngredientsList = req.body.IngredientsList;
-        console.log(IngredientsList[0])
+        console.log(IngredientsList[0].name)
         var RecipeMatch = await RecipeService.GetRecipesByIngredients(IngredientsList)
     }
 }
