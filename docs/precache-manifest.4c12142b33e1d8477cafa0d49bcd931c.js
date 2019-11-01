2a219df3423345c0c3da63577214db9adc175451
kyler <kyler@pop-os.localdomain>
2019-11-01 15:17:42 -0600

    building the react app for the website

---
+++ docs/precache-manifest.4c12142b33e1d8477cafa0d49bcd931c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14339757622b06acd243635c99e81ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c4da7e25034db9e44e5",
+    "url": "./static/css/main.4c82ffde.chunk.css"
+  },
+  {
+    "revision": "a1527f8d0f11a1ee6ab9",
+    "url": "./static/js/2.8fd1fd63.chunk.js"
+  },
+  {
+    "revision": "3c4da7e25034db9e44e5",
+    "url": "./static/js/main.0517729e.chunk.js"
+  },
+  {
+    "revision": "33229f4683baf6a73d2b",
+    "url": "./static/js/runtime-main.6c39e5e0.js"
+  }
+]);
