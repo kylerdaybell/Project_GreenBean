e4f1eb06eba67a84c2bfcd823f2caf99efb99f82
kyler daybell <relyk.photography.kyler@gmail.com>
2019-11-08 14:39:01 -0700

    working on the images

--- Api/services/ImageService.js
+++ Api/services/ImageService.js
@@ -9,6 +9,7 @@ var ImageService = {
         var imageInfo = base64ToImage(encodedstring,path);
         baseurl = "https://api.greenbeancooking.com/images/"
         imageurl = baseurl+imageInfo.fileName;
+        console.log(imageInfo.path)
         return imageurl;
     }
 }
