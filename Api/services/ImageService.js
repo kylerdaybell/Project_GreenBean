6e0ae8d98e816a080b50cfc956c83f985242a3eb
Kyler Daybell <kyler.daybell@ad.snow.edu>
2019-11-08 13:39:58 -0700

    working on the image upload

--- Api/services/ImageService.js
+++ Api/services/ImageService.js
@@ -4,17 +4,53 @@ var base64ToImage = require('base64-to-image');
 var ImageService = {
     Convert64BitToImageUrl: async function(encodedstring){
         path = "../Images/" 
-        var imageInfo =  await base64ToImage(encodedstring,path);
+        var imageInfo =  await this.base64ToImage(encodedstring,path);
         baseurl = "https://api.greenbeancooking.com/images/"
         
         imageurl = baseurl+imageInfo.fileName;
-        console.log("trying to create images")
-        
-        // var test = path+imageInfo.fileName;
-        // file = fs.open(test)
-        fs.writeFileSync(imageInfo.file,encodedstring);
-
         return imageurl;
+    },
+    base64ToImage: function (base64Str, path, optionalObj) {
+
+        if (!base64Str || !path) {
+            throw new Error('Missing mandatory arguments base64 string and/or path string');
+        }
+    
+        var optionalObj = optionalObj || {};
+        var imageBuffer = this.decodeBase64Image(base64Str);
+        var imageType = optionalObj.type || imageBuffer.type || 'png';
+        var fileName = optionalObj.fileName || 'img-' + Date.now();
+        var abs;
+        var fileName = '' + fileName;
+    
+        if (fileName.indexOf('.') === -1) {
+            imageType = imageType.replace('image/', '');
+            fileName = fileName + '.' + imageType;
+        }
+    
+        abs = path + fileName;
+        fs.writeFile(abs, imageBuffer.data, 'base64', function(err) {
+            if (err && optionalObj.debug) {
+                console.log("File image write error", err);
+            }
+    
+        });
+        return {
+            'imageType': imageType,
+            'fileName': fileName
+        };
+    },
+    decodeBase64Image: function(base64Str) {
+        var matches = base64Str.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        var image = {};
+        if (!matches || matches.length !== 3) {
+            throw new Error('Invalid base64 string');
+        }
+    
+        image.type = matches[1];
+        image.data = new Buffer(matches[2], 'base64');
+    
+        return image;
     }
 }
 
