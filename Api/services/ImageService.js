1ceef0e58937d9b2735b662688842bebb012ff55
kyler daybell <relyk.photography.kyler@gmail.com>
2019-11-08 15:22:55 -0700

    something is broken with recips

--- Api/services/ImageService.js
+++ Api/services/ImageService.js
@@ -3,13 +3,13 @@ var base64ToImage = require('base64-to-image');
 
 var ImageService = {
     Convert64BitToImageUrl: function(encodedstring){
-        var imageurl;
+        var imageurl = "https://api.greenbeancooking.com/images/DefaultImage.jpg";
         if(encodedstring == ""){
-            imageurl == "https://api.greenbeancooking.com/images/DefaultImage.jpg"
+            return;
         }else{
             console.log("we are in the darn function")
             path = "/root/Project_GreenBean/Api/Images/"
-            var imageInfo = base64ToImage(encodedstring,path);
+            var imageInfo = await base64ToImage(encodedstring,path);
             baseurl = "https://api.greenbeancooking.com/images/"
             imageurl = baseurl+imageInfo.fileName;
             console.log(imageInfo.abs)
