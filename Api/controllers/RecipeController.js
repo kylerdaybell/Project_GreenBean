2765410b24f456ccb3e9a5e85e05032334d7e9ac
Kyler Daybell <kyler.daybell@ad.snow.edu>
2019-11-08 13:01:48 -0700

    working on the image upload

--- Api/controllers/RecipeController.js
+++ Api/controllers/RecipeController.js
@@ -14,7 +14,10 @@ RecipeService.constructor(ApiDatabaseService, UserService,IngredientService);
 
 var RecipeController = {
     PostCreateRecipe: async function(req, res){
-        var recipe = new Recipe(null, null, req.body.name, req.body.descr, req.body.picture, req.body.preptime, 
+        if(req.body.picture != ""){
+            var ImageUrl = await ImageService.Convert64BitToImageUrl(req.body.picture)
+        }
+        var recipe = new Recipe(null, null, req.body.name, req.body.descr, ImageUrl, req.body.preptime, 
             req.body.cooktime, req.body.instructions, req.body.ingredientslist);
         var user = new User(req.body.email, req.body.password, null);
         if (await RecipeService.CreateRecipe(recipe, user)){
--- Api/services/ImageService.js
+++ Api/services/ImageService.js
@@ -1,13 +1,12 @@
 var base64ToImage = require('base64-to-image');
 
 var ImageService = {
-    Convert64BitToImageFile: function(encodedstring){
+    Convert64BitToImageUrl: function(encodedstring){
         path = "../Images/"
         var imageInfo = base64ToImage(encodedstring,path);
         baseurl = "https://api.greenbeancooking.com/images/"
         imageurl = baseurl+imageInfo.fileName;
         return imageurl;
-
     }
 }
 
