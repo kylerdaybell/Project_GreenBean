5e4100bdccec3138442ed029ba10cb373ae518be
kyler daybell <relyk.photography.kyler@gmail.com>
2019-11-08 15:16:07 -0700

    this is working

--- Api/services/ImageService.js
+++ Api/services/ImageService.js
@@ -3,15 +3,23 @@ var base64ToImage = require('base64-to-image');
 
 var ImageService = {
     Convert64BitToImageUrl: function(encodedstring){
-        console.log("we are in the darn function")
-        path = "/root/Project_GreenBean/Api/Images/"
-        var imageInfo = base64ToImage(encodedstring,path);
-        baseurl = "https://api.greenbeancooking.com/images/"
-        imageurl = baseurl+imageInfo.fileName;
-        console.log(imageInfo.abs)
+        var imageurl;
+        if(encodedstring == ""){
+            imageurl == "https://api.greenbeancooking.com/images/DefaultImage.jpg"
+        }else{
+            console.log("we are in the darn function")
+            path = "/root/Project_GreenBean/Api/Images/"
+            var imageInfo = base64ToImage(encodedstring,path);
+            baseurl = "https://api.greenbeancooking.com/images/"
+            imageurl = baseurl+imageInfo.fileName;
+            console.log(imageInfo.abs)
+        }
         return imageurl;
     },
     RemoveImage: function(imageurl){
+        let imagepatharray = imageurl.split('/')
+        imagepath = "/root/Project_GreenBean/Api/Images/"+imagepatharray[imagepatharray.length()]
+        fs.unlink(imagepath);
         return;
     }
 }
