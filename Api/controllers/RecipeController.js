ef0b2445367bb13917646b4fbdafc31cff8e5c15
kyler daybell <relyk.photography.kyler@gmail.com>
2019-11-08 14:53:10 -0700

    working on the images

--- Api/controllers/RecipeController.js
+++ Api/controllers/RecipeController.js
@@ -14,8 +14,8 @@ RecipeService.constructor(ApiDatabaseService, UserService,IngredientService);
 
 var RecipeController = {
     PostCreateRecipe: async function(req, res){
-        
         var ImageUrl = await ImageService.Convert64BitToImageUrl(req.body.picture);
+        console.log(ImageUrl)
         var recipe = new Recipe(null, null, req.body.name, req.body.descr, ImageUrl, req.body.preptime, 
             req.body.cooktime, req.body.instructions, req.body.ingredientslist);
         var user = new User(req.body.email, req.body.password, null);
