655f1efcdf1b5e97c4b28877acb3a7bbed945df1
kyler daybell <relyk.photography.kyler@gmail.com>
2019-11-08 16:22:15 -0700

    working to improve the look of the search page

--- App/src/components/searchPage/SearchPage.js
+++ App/src/components/searchPage/SearchPage.js
@@ -23,12 +23,13 @@ const SearchPage = () => {
       <button onClick={()=>SearchAPIByName()} className="w3-button w3-green ">search</button> */}
       <input id="ingredientSearchBox" className="w3-input w3-border" type="text" placeholder="search"/>
       <button onClick={()=>SearchAPIByIngredient()} className="w3-button w3-green ">search</button>
-      <div>
+      <div className="w3-row-padding">
         {recipeResults.map((recipe, index) => (
-          <div key={index}>
-            <h2>{recipe.recipe.name}</h2>
-            <p className="w3-text-green">Percent Match {recipe.percentmatch}</p>
-            <p>Description{recipe.recipe.descr}</p>
+          <div className="w3-card w3-third" key={index}>
+            <img src={recipe.recipe.picture} alt="recipe picture" style={{"width": "100%"}}></img>
+            <h2 className="w3-container">{recipe.recipe.name}</h2>
+            <p className="w3-text-green w3-container">Percent Match {recipe.percentmatch}</p>
+            <p className="w3-container">Description{recipe.recipe.descr}</p>
           </div>
         ))}
       </div>
