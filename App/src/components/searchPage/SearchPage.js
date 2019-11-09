2cf907d44050782192583a11bfc929f1be706d96
Brandon Isbell <brandonisbell1997@gmail.com>
2019-11-08 21:54:07 -0700

    cleaned up search page component

--- App/src/components/searchPage/SearchPage.js
+++ App/src/components/searchPage/SearchPage.js
@@ -1,31 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import PageTitle from "../shared/PageTitle";
-import GreenBeanAPIService from "../../Services/GreenBeanAPIService";
 import RecipeCard from "../shared/RecipeCard";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions";
 
 const SearchPage = props => {
-  const [recipeResults, setRecipeResults] = useState([]);
-  const SearchAPIByName = async () => {
-    let SearchTerm = document.getElementById("nameSearchBox").value;
-    let recipes = await GreenBeanAPIService.SearchForRecipeByName(SearchTerm);
-    setRecipeResults(recipes);
-  };
-  const SearchAPIByIngredient = async () => {
-    let SearchTerm = document.getElementById("ingredientSearchBox").value;
-    let recipes = await GreenBeanAPIService.SearchForRecipeByIngredient(
-      SearchTerm
-    );
-    setRecipeResults(recipes);
-  };
   return (
     <>
       <PageTitle title={"Search Page"} />
       <div id="content-area" className="w3-container"></div>
-      {/* <input id="nameSearchBox" className="w3-input w3-border" type="text" placeholder="search"/>
-      <button onClick={()=>SearchAPIByName()} className="w3-button w3-green ">search</button> */}
       <input
         id="ingredientSearchBox"
         className="w3-input w3-border"
