1d01b2a2c9ad0690de2f801bfc1c0a3b968b9a13
Brandon Isbell <brandonisbell1997@gmail.com>
2019-11-13 22:08:16 -0700

    linked add recipe page to redux

--- App/src/components/addRecipePage/AddRecipe.js
+++ App/src/components/addRecipePage/AddRecipe.js
@@ -1,8 +1,14 @@
 import React from "react";
 import PageTitle from "../shared/PageTitle";
 import AddRecipeForm from "./AddRecipeForm";
+import {Redirect} from "react-router-dom";
+import { connect } from "react-redux";
 
-const AddRecipe = () => {
+const AddRecipe = props => {
+  if (props.credentials.loggedIn === false) {
+    return <Redirect to="/login"/>;
+  }
+  
   return (
     <>
       <PageTitle title={"Add a New Recipe"}/>
@@ -11,4 +17,10 @@ const AddRecipe = () => {
   );
 };
 
-export default AddRecipe;
+const mapStateToProps = state => {
+  return {
+      credentials: state.greenBeanAPI.credentials
+  }
+}
+
+export default connect(mapStateToProps, null)(AddRecipe);
--- App/src/components/addRecipePage/AddRecipeForm.js
+++ App/src/components/addRecipePage/AddRecipeForm.js
@@ -6,60 +6,73 @@ import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions";
 
 const AddRecipeForm = props => {
-  var image = ""
-  const FilesAdded = (event)=>{
-    console.log("helppppp")
+  let image = "";
+
+  const FilesAdded = event => {
     let files = document.getElementById("picture").files[0];
-    var reader = new FileReader();
-    console.log(files)
+    let reader = new FileReader();
     reader.readAsDataURL(files);
-    
-    reader.onload = function(){
+
+    reader.onload = function() {
       image = reader.result;
-      console.log(image)
-    }
-  }
-  const AddRecipe = (theIngredientList) => {
+    };
+  };
+
+  const AddRecipe = theIngredientList => {
     let name = document.getElementById("name").value;
-    let descr = document.getElementById('description').value;
-    let preptime= document.getElementById('prep-time').value;
-    let cooktime= document.getElementById('cook-time').value;
-    let instructions = document.getElementById('instructions').value;
-    let email = "kyler.daybell96@gmail.com"
-    let password = "9479"
+    let descr = document.getElementById("description").value;
+    let preptime = document.getElementById("prep-time").value;
+    let cooktime = document.getElementById("cook-time").value;
+    let instructions = document.getElementById("instructions").value;
     let ingredientsList = theIngredientList;
-    let recipeSubmission = new RecipeAddModel(name,descr,image,preptime,cooktime,instructions,ingredientsList,email,password );
+    let recipeSubmission = new RecipeAddModel(
+      name,
+      descr,
+      image,
+      preptime,
+      cooktime,
+      instructions,
+      ingredientsList,
+      props.credentials.email,
+      props.credentials.password
+    );
     let recipeJson = JSON.stringify(recipeSubmission);
     props.CreateNewRecipe(recipeJson);
-  }
+  };
+
   return (
     <>
-        <legend>Add a new Recipe:</legend>
-        <label htmlFor="name">Name:</label>
-        <input className="w3-input w3-border" id="name" type="text" />
-        <br />
-        <label htmlFor="description">Description:</label>
-        <input className="w3-input w3-border" id="description" type="text" />
-        <br />
-        <label htmlFor="picture" >Picture:</label>
-        <input id="picture" onChange={event=>FilesAdded(event)} type="file" />
-        <br />
-        <label htmlFor="prep-time">Prep-Time:</label>
-        <input className="w3-input w3-border" id="prep-time" type="text" />
-        <br />
-        <label htmlFor="cook-time">Cook-Time:</label>
-        <input className="w3-input w3-border" id="cook-time" type="text" />
-        <br />
-        <label htmlFor="instructions">Instructions:</label>
-        <input className="w3-input w3-border" id="instructions" type="text" />
-        <br />
-        
-        <IngredientAdd onSubmit={AddRecipe}/>
+      <legend>Add a new Recipe:</legend>
+      <label htmlFor="name">Name:</label>
+      <input className="w3-input w3-border" id="name" type="text" />
+      <br />
+      <label htmlFor="description">Description:</label>
+      <input className="w3-input w3-border" id="description" type="text" />
+      <br />
+      <label htmlFor="picture">Picture:</label>
+      <input id="picture" onChange={event => FilesAdded(event)} type="file" />
+      <br />
+      <label htmlFor="prep-time">Prep-Time:</label>
+      <input className="w3-input w3-border" id="prep-time" type="text" />
+      <br />
+      <label htmlFor="cook-time">Cook-Time:</label>
+      <input className="w3-input w3-border" id="cook-time" type="text" />
+      <br />
+      <label htmlFor="instructions">Instructions:</label>
+      <input className="w3-input w3-border" id="instructions" type="text" />
+      <br />
+
+      <IngredientAdd onSubmit={AddRecipe} />
     </>
   );
 };
 
-export default connect(
-  null,
-  dispatch => bindActionCreators(actionCreators,dispatch)
-  )(AddRecipeForm);
+const mapStateToProps = state => {
+  return {
+      credentials: state.greenBeanAPI.credentials
+  };
+};
+
+export default connect(mapStateToProps, dispatch =>
+  bindActionCreators(actionCreators, dispatch)
+)(AddRecipeForm);
