add9e4dd28211f3c6a83fd1e348ad44319fe20bf
Mike <mexvance04@gmail.com>
2019-11-14 14:00:30 -0700

    adding home page image

--- App/src/components/homePage/HomePage.js
+++ App/src/components/homePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../css/w3.css";
+import "../../css/main.css";
 import PageTitle from "../shared/PageTitle";
 
 const HomePage = () => {
   return (
     <>
       <PageTitle title={"Green Bean Cooking"}/>
+      <img src="../../../images/homePageBeans.jfif" className="titleImage"></img>
       <div id="content-area" className="w3-container">
         <p>this is where the home page goes</p>
       </div>
