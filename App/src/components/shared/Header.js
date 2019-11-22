d49d9b0f012a0b998559ecb79b918d48d188471d
Brandon Isbell <brandonisbell1997@gmail.com>
2019-11-22 14:00:25 -0700

    search enter functionality accross the board

--- App/src/components/shared/Header.js
+++ App/src/components/shared/Header.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {withRouter} from "react-router-dom"
+import { withRouter } from "react-router-dom";
 import "../../css/w3.css";
 import "../../css/main.css";
 import { bindActionCreators } from "redux";
@@ -8,29 +8,41 @@ import * as actionCreators from "../../store/actions";
 import RecipeCard from "../shared/RecipeCard";
 
 const Header = props => {
-return (
+  const submitIfEnter = event => {
+    if (event.key === "Enter") {
+      props.SearchForRecipeByName(event.target.value);
+    }
+  };
+  return (
     <>
-    <nav className="w3-bar navBar sideSpacer">
-      <div className="input-container" >
-        <i className="fa fa-search icon w3-white"></i>
-        <input className="input-field" type="text" placeholder="Search by Recipe Name" id="searchName"/>
-        <button className="w3-button navButton w3-green"
-        onClick={() => {
-          props.SearchForRecipeByName(
-            document.getElementById("searchName").value
-            
-          );
-          props.history.push('/searchByName');
-          }
-        }>Search</button>
-      </div>
-    </nav>
-    <div className="spacerBar"></div>
+      <nav className="w3-bar navBar sideSpacer">
+        <div className="input-container">
+          <i className="fa fa-search icon w3-white"></i>
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Search by Recipe Name"
+            id="searchName"
+            onKeyDown={(event)=>submitIfEnter(event)}
+          />
+          <button
+            className="w3-button navButton w3-green"
+            onClick={() => {
+              props.SearchForRecipeByName(
+                document.getElementById("searchName").value
+              );
+              props.history.push("/searchByName");
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </nav>
+      <div className="spacerBar"></div>
     </>
   );
 };
 
-export default connect(
-  null,
-  dispatch => bindActionCreators(actionCreators, dispatch)
+export default connect(null, dispatch =>
+  bindActionCreators(actionCreators, dispatch)
 )(withRouter(Header));
