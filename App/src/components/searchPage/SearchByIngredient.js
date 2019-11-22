9fca2e5220a370a42ae421abd7b9e0f6b568313b
Brandon Isbell <brandonisbell1997@gmail.com>
2019-11-22 12:27:48 -0700

    search by ingredient in progress

--- App/src/components/searchPage/SearchByIngredient.js
+++ App/src/components/searchPage/SearchByIngredient.js
@@ -1,34 +1,66 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PageTitle from "../shared/PageTitle";
 import RecipeCard from "../shared/RecipeCard";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions";
 const SearchPage = props => {
+  const [ingredientList, setIngredientList] = useState([]);
+  const inputRef = useRef(null);
+
+  useEffect(()=>{
+    inputRef.current.focus();
+  });
+
+  const checkTab = (event) => {
+    if(event.key === 'Tab' || event.key === ','){
+      setIngredientList([...ingredientList, event.target.value])
+      event.target.value="";
+    }
+  }
+
+  const deleteIngredient = (ingredient) => {
+    setIngredientList(ingredientList.filter(i=>i!==ingredient))
+  }
+
+  const searchForRecipeByIngredient = () => {
+    let searchParameters = "";
+    ingredientList.forEach(element => {
+      searchParameters = searchParameters.concat(element, ",");
+    });
+    props.SearchForRecipeByIngredient(searchParameters);
+  }
+
   return (
     <>
-    
       <PageTitle title={"Search By Ingredients"} />
       <div className="fitBody">
-      <div id="content-area" className="w3-container"></div>
-      <input
-        id="ingredientSearchBox"
-        className="w3-input w3-border"
-        type="text"
-        placeholder="search"
-      />
-      <button
-        onClick={() =>
-          props.SearchForRecipeByIngredient(
-            document.getElementById("ingredientSearchBox").value
-          )
-        }
-        className="w3-button w3-green ">search</button>
-      <div className="w3-row-padding">
-        {props.recipes.map((recipe, index) => (
-          <RecipeCard recipe={recipe} key={index} />
-        ))}
-      </div>
+        <div id="content-area" className="w3-container"></div>
+
+        <span>
+          {ingredientList.map((ingredient, key) => (
+            <span key={key} onClick={()=>deleteIngredient(ingredient)}>{ingredient}  </span>
+          ))}
+          <input
+            ref={inputRef}
+            id="ingredientSearchBox"
+            className="w3-input w3-border"
+            type="text"
+            placeholder="search"
+            onKeyDown={(event)=>checkTab(event)}
+          />
+        </span>
+        <button
+          onClick={() =>searchForRecipeByIngredient()}
+          className="w3-button w3-green "
+        >
+          search
+        </button>
+        <div className="w3-row-padding">
+          {props.recipes.map((recipe, index) => (
+            <RecipeCard recipe={recipe} key={index} />
+          ))}
+        </div>
       </div>
     </>
   );
@@ -40,7 +72,6 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(
-  mapStateToProps,
-  dispatch => bindActionCreators(actionCreators, dispatch)
+export default connect(mapStateToProps, dispatch =>
+  bindActionCreators(actionCreators, dispatch)
 )(SearchPage);
