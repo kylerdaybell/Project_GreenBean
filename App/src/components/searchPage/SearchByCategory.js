4d55c9cb2d1583034b91f8d2bbc24901f0dbfede
Brandon Isbell <brandonisbell1997@gmail.com>
2019-11-22 22:01:02 -0700

    search by category uses a dropdown

--- App/src/components/searchPage/SearchByCategory.js
+++ App/src/components/searchPage/SearchByCategory.js
@@ -2,6 +2,7 @@ import React from "react";
 import PageTitle from "../shared/PageTitle";
 import RecipeCard from "../shared/RecipeCard";
 import SearchBar from "../shared/SearchBar";
+import recipeCategories from "../addRecipePage/recipeCategories"
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions";
@@ -11,7 +12,20 @@ const SearchByCategory = props => {
     
       <PageTitle title={"Search By Category"} />
       <div className="fitBody">
-      <SearchBar searchFunction={props.SearchForRecipeByCategory} />
+      <select className="w3-select w3-border" id="category" name="option" >
+        <option value="" disabled selected>Category</option>
+        {recipeCategories.map((category, key)=> (
+          <option key={key} value={category}>{category}</option>
+        ))}
+      </select>
+      <button
+        onClick={() =>
+          props.SearchForRecipeByCategory(document.getElementById("category"))
+        }
+        className="w3-button w3-green "
+      >
+        search
+      </button>
       <div className="w3-row-padding">
         {props.recipes.map((recipe, index) => (
           <RecipeCard recipe={recipe} key={index} />
