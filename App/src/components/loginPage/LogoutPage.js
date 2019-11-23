c348e60c62b5073e29f198c3af89713849918706
Brandon Isbell <brandonisbell1997@gmail.com>
2019-11-22 18:25:52 -0700

    logout button logs you out

--- App/src/components/loginPage/LogoutPage.js
+++ App/src/components/loginPage/LogoutPage.js
@@ -1,44 +1,41 @@
-import React from 'react'
+import React, {useEffect} from "react";
 import "../../css/w3.css";
 import "../../css/form.css";
 import PageTitle from "../shared/PageTitle";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions";
-import {Redirect} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 const LogoutPage = props => {
-    if(props.credentials.loggedIn === false){
-        return <Redirect to="/login" />
+  useEffect(()=> {
+    if(props.credentials.loggedIn === true){
+      props.Logout();
     }
-    const logout = event => {
-        event.preventDefault();
-        props.Logout();
-      } 
+    const timeout = setTimeout(()=>{
+      props.history.push("/");
+    },3000);
+    return () => clearTimeout(timeout);
+  });
   return (
     <>
-      <PageTitle title={"Logout"} />
+      <PageTitle title={"Successfully Logged Out"} />
       <div
         id="content-area"
         className="w3-container w3-row w3-center w3-display-center formFit"
       >
-        <form className="w3-card w3-half" onSubmit={event => logout(event)}>
-          <button type="submit" className="w3-btn w3-green formButton">
-            logout
-          </button>
-        </form>
+        Returning Home...
       </div>
     </>
   );
 };
 
 const mapStateToProps = state => {
-    return {
-        credentials: state.greenBeanAPI.credentials
-    }
-}
+  return {
+    credentials: state.greenBeanAPI.credentials
+  };
+};
 
-export default connect(
-    mapStateToProps,
-    dispatch => bindActionCreators(actionCreators,dispatch)
-    )(LogoutPage);
+export default connect(mapStateToProps, dispatch =>
+  bindActionCreators(actionCreators, dispatch)
+)(withRouter(LogoutPage));
