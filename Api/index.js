dc9bf8156f4446f5a3b05a5546584dfa90e60bd1
Brandon Isbell <brandonisbell1997@gmail.com>
2019-11-26 17:48:45 -0700

    working on dev environment setup

--- Api/index.js
+++ Api/index.js
@@ -16,17 +16,16 @@ if(process.env.NODE_ENV !== "development"){
   const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.greenbeancooking.com/privkey.pem', 'utf8');
   const certificate = fs.readFileSync('/etc/letsencrypt/live/api.greenbeancooking.com/cert.pem', 'utf8');
   const ca = fs.readFileSync('/etc/letsencrypt/live/api.greenbeancooking.com/chain.pem', 'utf8');
-  
+
   const credentials = {
     key: privateKey,
     cert: certificate,
     ca: ca
   };
-  
-  
+
+
   const httpsServer = https.createServer(credentials, app);
 }
-
 
 
 var corsOptions={
@@ -110,12 +109,12 @@ app.post('/getRecipeAdvancedSearch',cors(corsOptions),function(req,res){
   recipeController.GetRecipeAdvancedSearch(req,res);
 })
 
-if(process.env.NODE_ENV !== "development"){
-  httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443');
+if(process.env.NODE_ENV === "development"){
+  app.listen(port, () => {
+    console.log("Development Server running on port 443");
   });
 }else{
-  app.listen(port, () => {
-    console.log("HTTPS Server running on port 443");
+  httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
   });
 }
