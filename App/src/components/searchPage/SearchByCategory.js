0347e6aa5bfcf181d6004420d6894efcafc2d53f
Mike <mexvance04@gmail.com>
2019-11-28 03:01:15 -0700

    adjusting layout of search by category button

--- App/src/components/searchPage/SearchByCategory.js
+++ App/src/components/searchPage/SearchByCategory.js
@@ -18,20 +18,26 @@ const SearchByCategory = props => {
     
       <PageTitle title={"Search By Category"} />
       <div className="fitBody"> 
+      <div className="searchBarLayout">
+      <span className="searchBarForm">
       <select className="w3-select w3-border" id="category" name="option" >
         <option value="" disabled selected>No Category Selected</option>
         {recipeCategories.map((category, key)=> (
           <option key={key} value={category[0]}>{category[0]}</option>
         ))}
       </select>
+      </span>
+      <span className="searchBarButton">
       <button
         onClick={() =>
           props.SearchForRecipeByCategory(document.getElementById("category").value)
         }
-        className="w3-button w3-green"
+        className="w3-green searchButton fa fa-search"
       >
-        search
+         Search
       </button>
+      </span>
+      </div>
       <div className="w3-row-padding">
         {props.recipes.map((recipe, index) => (
           <RecipeCard recipe={recipe} key={index} />
