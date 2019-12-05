612bbd4304cc5d3437cdcc367654a5c719d8b97d
Kyler Daybell <kyler.daybell@ad.snow.edu>
2019-12-05 15:15:27 -0700

    setup tests for receipe service

--- Api/tests/userservice.test.js
+++ Api/tests/userservice.test.js
@@ -5,7 +5,6 @@ var User = require('../models/UserModel.js');
 UserService.constructor(MockApiDatabaseService);
 
 test('UserService.LoginUser with correct username and password',async ()=>{
-    MockApiDatabaseService.GetUser();
     var user = new User("kyler.daybell96@gmail.com","password","user")
     expect(await UserService.LoginUser(user)).toBe(true);
 })
