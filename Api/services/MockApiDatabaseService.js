ddb9d8e0f1dad187b392686c58621d7ef5ecc86f
kyler daybell <kyler.daybell96@gmail.com>
2019-12-06 08:14:11 -0700

    Created tests for UserService.LoginUser

--- Api/services/MockApiDatabaseService.js
+++ Api/services/MockApiDatabaseService.js
@@ -35,7 +35,8 @@ var MockApiDatabaseService = {
         
     },
     GetAllRecipes: async function(){
-        
+        result = [[{ ID: 3, USER_ID: 1, NAME: 'Walnut Gorgonzola Salad',DESCRIPTION: "A yummy, easy salad with candied walnuts, cranberries, Gorgonzola cheese, mixed greens, and a raspberry vinaigrette. It's always a big hit and is requested by my friends and family constantly! Even my picky fiance!", PICTURE: 'https://api.greenbeancooking.com/images/img-1573759586023.jpeg', PREP_TIME: '0:05:00', COOK_TIME: '0:05:00',INSTRUCTIONS: 'Place walnuts and sugar in a skillet over medium heat, stirring constantly until the sugar dissolves into a light brown liquid and coats the walnuts. Remove walnuts from skillet, and spread them out on a sheet of aluminum foil to cool. Place in a large salad bowl the mixed greens, cranberries, cheese, vinaigrette, vinegar, and olive oil. Toss gently; add candied walnuts, and toss again.',CATEGORY: null}]];
+        return result;
     },
     GetTopTenRecipes: async function(){
         
