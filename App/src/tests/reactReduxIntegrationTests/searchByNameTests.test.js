4d6ed5913828ba83baadbdf0149349a7d7bbbfcc
Brandon Isbell <brandonisbell1997@gmail.com>
2019-12-07 08:16:55 -0700

    Added another Integration Test for SearchByName. Created testUtils folder

--- App/src/tests/reactReduxIntegrationTests/searchByNameTests.test.js
+++ App/src/tests/reactReduxIntegrationTests/searchByNameTests.test.js
@@ -1,42 +1,55 @@
 import React from "react";
 import fetchMock from "fetch-mock";
-import Adapter from "enzyme-adapter-react-16";
-import { mount, configure } from "enzyme";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
 import SearchByNamePage from "../../components/searchPage/SearchByNamePage";
 import recipesByIngredientMock from "../../testMockData/mockData";
 import Store from "../../store/store.js";
+import { mountWithProviderAndRouter } from "../../testUtils/integrationTestUtils";
 
 const store = Store();
-configure({ adapter: new Adapter() });
 
 //empty offlineAPI mock for actions
 jest.mock("../../Services/OfflineAPI.js", () => () => ({}));
 
-function render() {
-  return mount(
-    <Provider store={store}>
-      <MemoryRouter>
-        <SearchByNamePage />
-      </MemoryRouter>
-    </Provider>
-  );
-}
-
-test("Search Button Pressed. Recipes get Added to Store", async () => {
-  fetchMock.mock("*", {
-    body: recipesByIngredientMock,
-    headers: { "content-type": "application.json" }
+describe("searchByName Component/Redux Integration Tests", () => {
+  afterEach(()=> {
+        fetchMock.restore();
   });
 
-  const expected = recipesByIngredientMock;
+  test("Search Button Pressed. Recipes get Added to Store", async () => {
+    fetchMock.mock("*", {
+      body: recipesByIngredientMock,
+      headers: { "content-type": "application.json" }
+    });
+  
+    const expected = recipesByIngredientMock;
+  
+    const wrapper = mountWithProviderAndRouter(<SearchByNamePage/>, store);
+    const searchBoxWrapper = wrapper.find("#searchBox");
+    const searchButtonWrapper = wrapper.find("#searchButton");
+  
+    searchBoxWrapper.instance().value = "testName";
+    searchButtonWrapper.simulate("click");
+  
+    await Promise.all([fetchMock.flush(false)]);
+    expect(store.getState().greenBeanAPI.recipes).toEqual(expected);
+  });
 
-  let wrapper = render();
-  wrapper
-    .find("#searchBox")
-    .simulate("change", { target: { value: "testName" } });
-  wrapper.find("#searchButton").simulate("click");
-  await Promise.all([fetchMock.flush(false)]);
-  expect(store.getState().greenBeanAPI.recipes).toEqual(expected);
-});
+  test("Enter Key Pressed. Recipes get Added to Store", async () => {
+    fetchMock.mock("*", {
+      body: recipesByIngredientMock,
+      headers: { "content-type": "application.json" }
+    });
+  
+    const expected = recipesByIngredientMock;
+  
+    const wrapper = mountWithProviderAndRouter(<SearchByNamePage/>, store);
+    const searchBoxWrapper = wrapper.find("#searchBox");
+  
+    searchBoxWrapper.instance().value = "testName";
+    searchBoxWrapper.simulate('keypress', {key: 'Enter'});
+  
+    await Promise.all([fetchMock.flush(false)]);
+    expect(store.getState().greenBeanAPI.recipes).toEqual(expected);
+  });
+})
+
