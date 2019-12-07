f5db51b5c53cbaaa7a0d49a9e590e0c4e4dc4230
Brandon Isbell <brandonisbell1997@gmail.com>
2019-12-07 09:09:26 -0700

    Added integration tests for SearchByIngredient and ChamgeMode on settings page.

---
+++ App/src/tests/reactReduxIntegrationTests/settingsPageTests.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import fetchMock from "fetch-mock";
+import SettingsPage from "../../components/settings/SettingsPage";
+import recipesByIngredientMock from "../../testMockData/mockData";
+import Store from "../../store/store.js";
+import { mountWithProviderAndRouter } from "../../testUtils/integrationTestUtils";
+import {initialState} from "../../store/initialState";
+
+const store = Store();
+
+//empty offlineAPI mock for actions
+jest.mock("../../Services/OfflineAPI.js", () => () => ({}));
+
+describe("searchByCategory Component/Redux Integration Tests", () => {
+  afterEach(()=> {
+        fetchMock.restore();
+  });
+
+  test.each([true, false])("Change Mode Button Pressed. Mode starts as %s. Mode is Changed in Store", (initial) => {
+    //setup custom initial state for this test
+    const testStore = Store({...initialState, greenBeanAPI: {...initialState.greenBeanAPI, offlineMode: initial}})
+  
+    const expected = !initial;
+  
+    const wrapper = mountWithProviderAndRouter(<SettingsPage/>, testStore);
+    const changeModeButton = wrapper.find("#changeModeButton");
+
+    changeModeButton.simulate("click");
+
+    expect(testStore.getState().greenBeanAPI.offlineMode).toEqual(expected);
+  });
+})
